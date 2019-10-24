e6e9d1e11831561f2304e9704adbbd34e082c72a
Sivarjuen Ravichandran <sivarjuen.ravichandran@hotmail.com>
2019-10-24 17:13:00 +0100

    Add ResourceManager integration

--- main.cpp
+++ main.cpp
@@ -1,17 +1,35 @@
 #include "RQ_Engine.h"
-int main(){
 
-    RQEngine::Window window("Mercy", 1280, 720, 120);
-    RQEngine::InputManager IM;
+using namespace RQEngine;
+
+int main()
+{
+    InputManager IM;
+    ResourceManager* res = new ResourceManager();
+
+    // Create the main window
+    //sf::RenderWindow window(sf::VideoMode(800, 600), "Mercy");
+	Window window("Mercy", 1280, 720, 120);
 
     b2Vec2 gravity(0.0f, -10.0f);
     b2World world(gravity);
-    int count = 0;
+	int count = 0;
 
-    // Start the game loop
-    while (window.isOpen()){
+	// Load resources
+	if (res->loadTexture("assets/textures/wall128x128.png", "wall", false)) {
+		printf("Loaded texture\n");
+	}
+	if (res->createSprite("s", "wall", 0, 0, 128, 128)) {
+		printf("Created sprite\n");
+	}
 
-        window.update();
+	sf::Sprite* sprite = ResourceManager::getSprite("s");
+
+	
+	// Game loop
+    while (window.isOpen())
+    {
+		window.update();
 
         if(IM.isKeyUp(KeyCode::MouseLeft)){
             std::cout << "MB1 Released" << std::endl;
@@ -24,10 +42,12 @@ int main(){
                 std::cout << "MB1 Pressed" << std::endl;
             }
         }
-        window.prepareFrame();
+
+		window.prepareFrame();
+		window.draw(*sprite);
         window.drawFrame();
     }
 
+	res->~ResourceManager();
     return EXIT_SUCCESS;
-
 }
